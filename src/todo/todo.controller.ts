ab790abe4364efbe3d5bfdc4d3fb54d825480c49
Rynhalt <marcus147@icloud.com>
2024-06-19 16:15:29 +0900

    DBへの接続/書き込み

---
+++ src/todo/todo.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Body, Post, Param } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTaskDto } from './create-task.dto';
+
+@Controller('todo')
+export class TodoController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get('list')
+  async getList() {
+    const result = await this.prisma.task.findMany({
+      where: {
+        is_done: false,
+      },
+    });
+    return [...result];
+  }
+
+  @Post('')
+  async add(@Body() task: CreateTaskDto) {
+    const result = await this.prisma.task.create({
+      data: task,
+    });
+    return {
+      status: 'OK',
+    };
+  }
+
+  @Post(':id/done')
+  async done(@Param() param) {
+    await this.prisma.task.update({
+      data: {
+        is_done: true,
+      },
+      where: {
+        id: parseInt(param.id),
+      },
+    });
+    return {
+      status: 'OK',
+    };
+  }
+}
